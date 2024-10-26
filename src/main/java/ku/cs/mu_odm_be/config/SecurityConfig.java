fa2c591e040f5583d9d9c6a3d8a8ae8b89d7fbd9
xeff09 <wongchaita03@gmail.com>
2024-10-26 15:11:59 +0700

    fix: cors frontend permission

--- src/main/java/ku/cs/mu_odm_be/config/SecurityConfig.java
+++ src/main/java/ku/cs/mu_odm_be/config/SecurityConfig.java
@@ -70,7 +70,7 @@ public class SecurityConfig {
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(List.of("*")); // Allow frontend origin
+    configuration.setAllowedOrigins(List.of("http://localhost:3000")); // Allow frontend origin
     configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
     configuration.setAllowedHeaders(List.of("*")); // Allow any headers
     configuration.setAllowCredentials(true); // Allow credentials (e.g., cookies)
