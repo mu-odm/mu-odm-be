3ee5a57c754e7e836750316378757320ffccac14
xeff09 <wongchaita03@gmail.com>
2024-11-01 14:55:23 +0700

    add: cors allow for vercel

--- src/main/java/ku/cs/mu_odm_be/MuOdmBeApplication.java
+++ src/main/java/ku/cs/mu_odm_be/MuOdmBeApplication.java
@@ -21,7 +21,7 @@ public class MuOdmBeApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:3000")
+						.allowedOrigins("https://muodm-deploy-kd4vrzwif-xeff09s-projects.vercel.app", "http://localhost:3000")
 						.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
 						.allowedHeaders("*")
 						.allowCredentials(true);
